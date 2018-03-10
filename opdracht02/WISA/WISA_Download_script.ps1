b4d6957a6074357830ac8bcb3280918ef834451b
joeri verhavert <joeri.verhavert.y7236@student.hogent.be>
2018-03-10 12:33:43 +0100

    uploaden van download script wisa

---
+++ opdracht02/WISA/WISA_Download_script.ps1
@@ -0,0 +1,31 @@
+﻿Clear-Host;
+
+Import-Module ServerManager;
+Import-Module PackageManagement
+
+#controleren op windows opdates
+Clear-Host;
+Write-Host("Uw windowsmachine wordt gecontroleerd op update. Even geduldt aub.");
+
+cd C:\Windows\System32;
+wuaclt.exe /detectnow;
+wuaclt.exe /updatenow;
+
+#installatie IIS + installatie http + asp.net;
+clear-host;
+Write-Host("IIS module wordt momenteel gëinstalleerd");
+add-windowsfeature web-server -includeallsubfeature;
+Add-WindowsFeature NET-HTTP-Activation;
+
+#installatie mySQL
+clear-host;
+Write-Host("mySQL  wordt momenteel gëinstalleerd");
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+choco feature enable -n allowGlobalConfirmation;
+choco install mysql -y;
+choco update mysql;
+
+#installaties compleet
+clear-host;
+Write-Host("ALLES IS GËINSTALLEERD!");
+
